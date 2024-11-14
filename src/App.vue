01382062938aa800066f08b4d6e91257031ef101
Bartek Kozdrowiecki <bkozdrowiecki@divante.pl>
2024-11-14 14:29:39 +0100

    feat: add basic auth

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,50 @@
 <template>
-  <template v-if="device.mobile">
-    <main class="mb-16 box-border">
-      <RouterView />
-    </main>
-    
-    <NavBar />
+  <template v-if="isAuthenticated">
+    <template v-if="device.mobile">
+      <main class="mb-16 box-border">
+        <RouterView />
+      </main>
+      
+      <NavBar />
+    </template>
+  
+    <div v-else class="h-screen w-full flex items-center justify-center">
+      <h1 v-text="'Please use mobile device.'" />
+    </div>
   </template>
 
-  <div v-else class="h-screen w-full flex items-center justify-center">
-    <h1 v-text="'Please use mobile device.'" />
-  </div>
+  <div v-else class="w-full h-screen flex items-center justify-center text-center text-2xl" v-text="'Please refresh the page.'" />
 </template>
 
 <script setup>
+import { ref, onMounted } from 'vue'
 import { useDevice } from 'next-vue-device-detector'
 
 import NavBar from '@/components/NavBar.vue'
 
 const device = useDevice()
+
+const isAuthenticated = ref(false)
+
+const authenticateUser = () => {
+  const username = prompt('Enter your username:')
+  const password = prompt('Enter your password:')
+
+  if (username === process.env.VUE_APP_AUTH_USERNAME && password === process.env.VUE_APP_AUTH_PASSWORD) {
+    isAuthenticated.value = true
+    localStorage.setItem('isAuthenticated', true)
+  } else {
+    alert('Invalid credentials. Please refresh and try again.')
+  }
+}
+
+onMounted(() => {
+  if (localStorage.getItem('isAuthenticated')) {
+    isAuthenticated.value = true
+  } else {
+    authenticateUser()
+  }
+})
 </script>
 
 <style lang="scss">
